672581dd9ab3344c54c0e1c3f24d87d2fcc6835d
zhanghao <506365426@qq.com>
2020-02-20 15:56:11 +0800

    test:

---
+++ test/form/tools/DateTimePicker/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <fast-date-time-picker
+      :value="value"
+      :editable="editable"
+      :clearable="clearable"
+      :size="size"
+      :placeholder="placeholder"
+      :type="type"
+      :align="align"
+      :is-render="isRender"
+			@dateChange="dateChangeEvent"
+			:listeners="listeners"
+    />
+		<!-- <fast-date-time-picker v-model="value" /> -->
+  </div>
+</template>
+
+<script>
+import FastDateTimePicker from '../common/form/tools/date-time-picker.js'
+
+export default {
+  components: {
+    FastDateTimePicker
+	},
+	/* watch: {
+    value (value, oldValue) {
+			// 第二个示例 v-model的演示
+      console.info('value=', value, 'oldValue=', oldValue);
+    }
+  }, */
+  data() {
+    this.listeners = {
+			focus: (event) => {
+        console.info('focus ', event);
+      },
+      change: (value) => {
+        console.info('change ', value)
+      },
+      dateChange: (value) => {
+				// dateChangeEvent 方法将不会触发，会触发到这里
+        console.info('v-model ', value)
+      }
+    }
+    return {
+      	value: new Date(),
+        editable: false,
+        clearable: false,
+        size: 'mini',
+        placeholder: '请选择',
+        type: 'date',
+        align: 'right',
+        isRender: true
+    }
+  },
+  methods: {
+    dateChangeEvent (value) {
+      console.info(value);
+		}
+  }
+};
+</script>
+
+<style></style>
