b10e731554268cf424d3a55e678231b240fde553
zhanghao <506365426@qq.com>
2020-02-20 10:49:58 +0800

    test:

---
+++ test/form/tools/TextArea/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+      <fast-text-area
+        :value="value"
+        :rows="rows"
+        :listeners="listeners"
+        @focus="focusEvent"
+      />
+  </div>
+</template>
+
+<script>
+import FastTextArea from '../common/form/tools/text-area.js'
+
+export default {
+  components: {
+    FastTextArea
+  },
+  data () {
+    this.listeners = {
+      inputChange: this.inputChangeHandler
+    }
+    return {
+      value: `hello input-${Math.ceil(Math.random() * 1000)}`,
+      rows: 5
+    }
+  },
+  methods: {
+    inputChangeHandler (value) {
+      // v-mode 事件
+      console.info('textArea 值发生改变时触发：', value)
+    },
+    focusEvent (event) {
+      console.info(event);
+    }
+  }
+}
+</script>
+
+<style></style>
