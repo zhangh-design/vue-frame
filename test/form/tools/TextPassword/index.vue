226ff873da60dc9c0ad52e92559f7db134f5effe
zhanghao <506365426@qq.com>
2020-02-20 11:11:43 +0800

    test:

---
+++ test/form/tools/TextPassword/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <fast-text-password :value="value" clearable @inputChange="inputChangeEvent" :slotNode="slotNode" :listeners="listeners">
+      <template v-slot:prepend>
+        <em>密码：</em>
+      </template>
+    </fast-text-password>
+  </div>
+</template>
+
+<script>
+import FastTextPassword from "../common/form/tools/text-password.js";
+
+export default {
+  components: {
+    FastTextPassword
+  },
+  data() {
+    this.listeners = {
+      focus: () => {
+        console.info("focus ", event);
+      }
+    }
+    this.slotNode = {
+      template: "<em>取消</em>"
+    }
+    return {
+      value: `123456`
+    }
+  },
+  methods: {
+    inputChangeEvent(value) {
+      console.info(value);
+    }
+  }
+};
+</script>
+
+<style></style>
