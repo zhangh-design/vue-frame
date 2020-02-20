f4acea7202c7f013e458de10985b49cc5fd0e217
zhanghao <506365426@qq.com>
2020-02-20 11:22:18 +0800

    test:

---
+++ test/form/tools/TextHidden/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <fast-text-hidden
+        :value="value"
+      />
+  </div>
+</template>
+
+<script>
+import FastTextHidden from '../common/form/tools/text-hidden.js'
+
+export default {
+  components: {
+    FastTextHidden
+  },
+  data() {
+    return {
+      value: `1001`
+    }
+  }
+};
+</script>
+
+<style></style>
