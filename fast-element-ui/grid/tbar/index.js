d69508cfba2862125cbaed8935e39260daee7228
zhangh <506365426@qq.com>
2020-03-23 12:54:51 +0800

    feat: 增加 tbar 工具栏插槽

--- fast-element-ui/grid/tbar/index.js
+++ fast-element-ui/grid/tbar/index.js
@@ -3,8 +3,15 @@
  * 默认位于查询栏下方
  */
 const FastTBar = {
+  inject: ['getFastGrid'],
   render (h) {
-    return h('div', '默认位于工具栏下方')
+    return h(
+      'div',
+      {
+        style: 'height: 100%;'
+      },
+      [this.$slots.default, this.$scopedSlots.tbarScope()]
+    )
   }
 }
 export default FastTBar
