533cef453fce0a79487947dca3a7f2c09ca109c8
zhangh <506365426@qq.com>
2020-03-23 12:54:04 +0800

    feat: 设置查询栏插槽

--- fast-element-ui/grid/search/index.js
+++ fast-element-ui/grid/search/index.js
@@ -1,31 +1,16 @@
 /**
  * search 面板
  */
-import FastForm from '../../form/index.js'
-import _has from 'lodash/has'
-
 const FastGridSearch = {
-  components: {
-    FastForm
-  },
   inject: ['getFastGrid'],
-  props: {
-    // 配置详情
-    detail: {
-      type: Array,
-      default () {
-        return []
-      }
-    }
-  },
   render (h) {
-    console.info('aaaa', this.$slots.default);
-    if (_has(this.$slots, 'default')) {
-      return h('div', { style: 'height: 100%;' }, [this.$slots.default])
-    } else {
-      console.info('渲染配置参数');
-      return h('div', ['渲染配置参数'])
-    }
+    return h(
+      'div',
+      {
+        style: 'height: 100%;'
+      },
+      [this.$slots.default, this.$scopedSlots.searchScope()]
+    )
   }
 }
 export default FastGridSearch
