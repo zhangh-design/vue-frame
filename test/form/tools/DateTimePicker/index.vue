3812826b70e5e5e66c63a5aeb3579944601accab
zhangh <506365426@qq.com>
2020-04-02 17:48:53 +0800

    style:

--- test/form/tools/DateTimePicker/index.vue
+++ test/form/tools/DateTimePicker/index.vue
@@ -9,10 +9,10 @@
       :type="type"
       :align="align"
       :is-render="isRender"
-			@dateChange="dateChangeEvent"
-			:listeners="listeners"
+      @dateChange="dateChangeEvent"
+      :listeners="listeners"
     />
-		<!-- <fast-date-time-picker v-model="value" /> -->
+    <!-- <fast-date-time-picker v-model="value" /> -->
   </div>
 </template>
 
@@ -22,43 +22,43 @@ import FastDateTimePicker from '../common/form/tools/date-time-picker.js'
 export default {
   components: {
     FastDateTimePicker
-	},
-	/* watch: {
+  },
+  /* watch: {
     value (value, oldValue) {
-			// 第二个示例 v-model的演示
+      // 第二个示例 v-model的演示
       console.info('value=', value, 'oldValue=', oldValue);
     }
   }, */
-  data() {
+  data () {
     this.listeners = {
-			focus: (event) => {
+      focus: (event) => {
         console.info('focus ', event);
       },
       change: (value) => {
         console.info('change ', value)
       },
       dateChange: (value) => {
-				// dateChangeEvent 方法将不会触发，会触发到这里
-				console.info('v-model ', value)
-				this.value = value
+        // dateChangeEvent 方法将不会触发，会触发到这里
+        console.info('v-model ', value)
+        this.value = value
       }
     }
     return {
-      	value: new Date(),
-        editable: false,
-        clearable: false,
-        size: 'mini',
-        placeholder: '请选择',
-        type: 'date',
-        align: 'right',
-        isRender: true
+      value: new Date(),
+      editable: false,
+      clearable: false,
+      size: 'mini',
+      placeholder: '请选择',
+      type: 'datetime',
+      align: 'right',
+      isRender: true
     }
   },
   methods: {
     dateChangeEvent (value) {
-			console.info(value);
-			this.value = value
-		}
+      console.info(value);
+      this.value = value
+    }
   }
 };
 </script>
