b9693c26b7a402378951f1e8c1df19006589d9cb
zhangh <506365426@qq.com>
2020-05-15 09:55:14 +0800

    test:

---
+++ test/layout/double-wing.vue
@@ -0,0 +1,35 @@
+<template>
+  <div style="height: 50px;">
+    <fast-double-wing-layout :border="border">
+      <template v-slot:left>
+        <div style="padding: 10px;">
+          左侧
+        </div>
+      </template>
+      <template v-slot:middle>
+        <div>
+          中间
+        </div>
+      </template>
+      <template v-slot:right>
+        <div>
+          右侧-用户名-登出
+        </div>
+      </template>
+    </fast-double-wing-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      border: '1px solid blue'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
